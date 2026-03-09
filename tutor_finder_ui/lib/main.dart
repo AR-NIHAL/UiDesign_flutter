c28d21ca5c1dd8e02d4c0d6572142cf7136f3f63
Ashikur Rahman Nihal <arnihal.cs@gmail.com>
2026-03-09 21:38:49 +0600

    Update main.dart

--- tutor_finder_ui/lib/main.dart
+++ tutor_finder_ui/lib/main.dart
@@ -1,18 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:tutor_finder_ui/dir_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: TutorDirScreen(),
-    ); // This trailing comma makes auto-formatting nicer for build methods.
-  }
-}
